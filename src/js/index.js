9513c961a0d8bbdf2554c97bd028d1e5453f7c28
Matthew Gerring <gerring.matthew@gmail.com>
2018-01-22 19:10:24 -0800

    also reactstrap

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import SimpleChartParameter from './SimpleChart'
+import 'bootstrap/dist/css/bootstrap.css'
+
 
 ReactDOM.render(
     <div>
