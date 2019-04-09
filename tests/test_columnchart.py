8efe22b25d7d43390bde1b0b098d43e963632187
Adam Hooper <adam@adamhooper.com>
2019-04-09 16:06:21 -0400

    Column formats -- take 2
    Circumvents https://github.com/vega/vega/issues/1760
    
    Also, upgrades to Vega 5

--- tests/test_columnchart.py
+++ tests/test_columnchart.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from collections import namedtuple
 import unittest
 import pandas as pd
 from columnchart import render, migrate_params
@@ -12,6 +13,9 @@ DefaultParams = {
     'x_column': '',
     'y_columns': [],
 }
+
+
+Column = namedtuple('Column', ('name', 'type', 'format'))
 
 
 def P(**kwargs):
@@ -75,20 +79,31 @@ class MigrateParamsTest(unittest.TestCase):
 
 class IntegrationTest(unittest.TestCase):
     def test_happy_path(self):
-        dataframe, error, json_dict = render(pd.DataFrame({
-            'A': ['foo', 'bar'],
-            'B': [1, 2],
-            'C': [2, 3],
-        }), P(
-            x_column='A',
-            y_columns=[
-                {'column': 'B', 'color': '#bbbbbb'},
-                {'column': 'C', 'color': '#cccccc'},
-            ]
-        ))
+        dataframe, error, json_dict = render(
+            pd.DataFrame({
+                'A': ['foo', 'bar'],
+                'B': [1, 2],
+                'C': [2, 3],
+            }),
+            P(
+                x_column='A',
+                y_columns=[
+                    {'column': 'B', 'color': '#bbbbbb'},
+                    {'column': 'C', 'color': '#cccccc'},
+                ]
+            ),
+            input_columns={
+                'A': Column('A', 'text', None),
+                'B': Column('B', 'number', '{:,d}'),
+                'C': Column('C', 'number', '{:,f}'),
+            }
+        )
+        # Check values
         self.assertEqual(json_dict['data'][0]['values'], [
             {'bar': 'B', 'y': 1, 'group': 0, 'name': 'foo'},
             {'bar': 'B', 'y': 2, 'group': 1, 'name': 'bar'},
             {'bar': 'C', 'y': 2, 'group': 0, 'name': 'foo'},
             {'bar': 'C', 'y': 3, 'group': 1, 'name': 'bar'},
         ])
+        # Check axis format is first Y column's format
+        self.assertEqual(json_dict['axes'][1]['format'], ',d')
